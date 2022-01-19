af986d514a604abde6256a2b8027e44dbbf6d8c2
ArlBiern <arielbiernacki@gmail.com>
2022-01-19 14:54:18 +0100

    Add PageNotFound Copomonent

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,5 +1,19 @@
+import NotFound from "./svg/NotFound";
+
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="section">
+      <div className="notFound_cnt">
+        <div className="notFound_textCnt">
+          <h2>404</h2>
+          <h3>Page not found</h3>
+        </div>
+        <NotFound />
+        <p className="notFound_info">Look's like you reached a dead end!</p>
+        <p className="notFound_info">Please go back or go to the main site.</p>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;

6d127b54fa5e5905acc78eb6bc051ee0005c9da6
ArlBiern <arielbiernacki@gmail.com>
2022-01-24 13:22:00 +0100

    fixing github_pages routing bug

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/main_portfolio" element={<Home />} />
+        <Route path="/main_portfolio/*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
